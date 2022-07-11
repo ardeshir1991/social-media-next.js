5aa99aef55c2ef8afd1c36eedfd77980a741e429
ardeshir <ardeshiref1991@gmail.com>
2022-07-11 12:09:30 +0430

    adding post data and showing posts

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,11 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&family=Roboto:wght@100;300;400;500;700&family=Source+Serif+Pro:wght@400;600;700&display=swap"
           rel="stylesheet"
