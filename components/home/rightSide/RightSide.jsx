de253cbcf2eb96bb4a6fed1c4679a800441a7ebb
ardeshir <ardeshiref1991@gmail.com>
2022-07-12 13:04:48 +0430

    adding notification icons

--- components/home/rightSide/RightSide.jsx
+++ components/home/rightSide/RightSide.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import {
+  FaCog,
+  FaRegBell,
+  FaRegCommentDots,
+  FaRegEnvelope,
+} from "react-icons/fa";
+import styles from "./rightSide.module.css";
 
 const RightSide = () => {
   return (
-    <div>RightSide</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.notification}>
+        <div className={styles.item}>
+          <FaCog />
+          <span>2</span>
+        </div>
+        <div className={styles.item}>
+          <FaRegEnvelope />
+          <span>2</span>
+        </div>
+        <div className={styles.item}>
+          <FaRegCommentDots />
+          <span>2</span>
+        </div>
+        <div className={styles.item}>
+          <FaRegBell />
+          <span>2</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default RightSide
+export default RightSide;
